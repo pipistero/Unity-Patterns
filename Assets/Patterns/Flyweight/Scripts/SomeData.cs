9f83ad8c44db1cf5c2c3b458052eb6d0bfe45dfc
Anton Kazakevich <kazakevich.anton@luckymonkeys.games>
2023-10-06 17:59:17 +0300

    FlyweightPattern Done

---
+++ Assets/Patterns/Flyweight/Scripts/SomeData.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Patterns.Flyweight
+{
+    public class SomeData
+    {
+        private float _value1 = 122f;
+        private float _value2 = 122f;
+        private float _value3 = 122f;
+        private float _value4 = 122f;
+        private float _value5 = 122f;
+        private float _value6 = 122f;
+        private float _value7 = 122f;
+        private float _value8 = 122f;
+        private float _value9 = 122f;
+        private float _value10 = 122f;
+        private float _value11 = 122f;
+        private float _value12 = 122f;
+        private float _value13 = 122f;
+        private float _value14 = 122f;
+        private float _value15 = 122f;
+        private float _value16 = 122f;
+        private float _value17 = 122f;
+        private float _value18 = 122f;
+        private float _value19 = 122f;
+        private float _value20 = 122f;
+    }
+}
